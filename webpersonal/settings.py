6cb1b77eb250b7217e7aeca20b3855083202be13
jeshuamb <252979968+jeshuamb@users.noreply.github.com>
2026-06-11 19:10:59 -0400

    Mejoras en el diseño de la web-personal

--- webpersonal/settings.py
+++ webpersonal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0dg(!nexq5k(gkvzihn$dgoj&jrudll9h%+!oqq@p2-a-f)c2n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.1.106']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.1.104']
 
 
 # Application definition
